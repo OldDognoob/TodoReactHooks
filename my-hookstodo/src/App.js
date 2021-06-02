05aa8bc9b7113ee9ef486c4a2f008aff9f215c56
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-06-02 18:15:12 +0300

    wip app-form-file

--- my-hookstodo/src/App.js
+++ my-hookstodo/src/App.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./components/Header";
+import Form from "./components/Form";
 import './App.css';
 
 const App = () => {
+  const[input, setInput] = useState("");
+  const[todos, setTodos] = useState([]);
   return(<div className="container">
     <div className="app-wrapper">
       <div>
         <Header/>
+      </div>
+      <div>
+        <Form 
+        input={input}
+        setInput={setInput}
+        todos={todos}
+        setTodos={setTodos}
+        />
       </div>
     </div>
   </div>);
