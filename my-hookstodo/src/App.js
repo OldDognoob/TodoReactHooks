7c183c98985fe54c407fe8434bfa3176cb3c4ece
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-06-02 18:04:09 +0300

    wip-project

--- my-hookstodo/src/App.js
+++ my-hookstodo/src/App.js
@@ -1,12 +1,15 @@
-
+import React from "react";
+import Header from "./components/Header";
 import './App.css';
 
-function App() {
-  return (
-    <div>
-     Todo App
+const App = () => {
+  return(<div className="container">
+    <div className="app-wrapper">
+      <div>
+        <Header/>
+      </div>
     </div>
-  );
+  </div>);
 }
 
 export default App;
--- my-hookstodo/src/components/Header.js
+++ my-hookstodo/src/components/Header.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <div>
-            
+        <div className="header">
+            <h1>Todo App</h1>
         </div>
     )
 }
